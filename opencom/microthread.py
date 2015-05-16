5c253cd2601800403b9a8af4716394e4fd63d30a
EcmaXp <admin@ecmaxp.pe.kr>
2015-05-16 05:52:10 +0000

    opencom: module microthread changed. (subclassing is unsafe)

--- opencom/microthread.py
+++ opencom/microthread.py
@@ -1,28 +1,40 @@
-import umicrothread
-from umicrothread import MicroThread as _MicroThread
-from umicrothread import *
-
-assert STATUS_NORMAL
-assert STATUS_YIELD
-assert STATUS_EXCEPTION
-assert STATUS_LIMIT
-assert STATUS_PAUSE
-assert STATUS_FORCE_PAUSE
-assert STATUS_STOP
-assert STATUS_RUNNING
-assert LIMIT_SOFT
-assert LIMIT_HARD
+import umicrothread as _microthread
+from umicrothread import pause, current_thread as _current_thread, \
+    MicroThread as _MicroThread
+from umicrothread import STATUS_NORMAL, STATUS_YIELD, STATUS_EXCEPTION, \
+    STATUS_LIMIT, STATUS_PAUSE, STATUS_FORCE_PAUSE, STATUS_STOP, \
+    STATUS_RUNNING, LIMIT_SOFT, LIMIT_HARD
 
 def auto(*args, **kwargs):
     def warp(func):
         return MicroThread(func.__name__, func, *args, **kwargs)
     return warp
 
-class MicroThread(_MicroThread):
-    def __init__(self, name, function, *args, **kwargs):
-        pass
+_active = None
 
-    """
+def current_thread():
+    raw_thread = _current_thread()
+    thread = _active
+
+    if raw_thread is None:
+        assert _active is None
+    else:
+        assert thread._thread is raw_thread
+
+    return thread
+
+class MicroThread():
+    def __init__(self, name, function, *args, **kwargs):
+        self._thread = _MicroThread(name, function, *args, **kwargs)
+        
+    def __repr__(self):
+        return "<%s name=%r, function=%r>" % (type(self).__name__, self.name, self.function)
+    
+    def __dir__(self):
+        return dir(self._thread)
+    
+    # __setattr__ are not exists.
+
     @property
     def name(self):
         return self._thread.name
@@ -30,10 +42,6 @@ class MicroThread(_MicroThread):
     @property
     def function(self):
         return self._thread.function
-    
-    @property
-    def prev_thread(self):
-        return self._thread.prev_thread
     
     @property
     def cpu_hard_limit(self):
@@ -66,16 +74,7 @@ class MicroThread(_MicroThread):
     @cpu_current_executed.setter
     def cpu_current_executed(self, value):
         self._thread.cpu_current_executed = value
-    """
-
-    def __repr__(self):
-        return "<%s name=%r, function=%r>" % (type(self).__name__, self.name, self.function)
     
-    #def __dir__(self):
-    #    return dir(self._thread)
-    
-    # __setattr__ are not exists.
-
     # del cpu_* value will set zero and unlimited.
     # assign with zero is not allowed?
     
@@ -83,6 +82,14 @@ class MicroThread(_MicroThread):
         return self.resume(value)
     
     def resume(self, value=None):
-        status, result = super().resume(value)
+        global _active
+        prev_thread = _active
+        thread = self._thread
 
+        try:
+            _active = self
+            status, result = thread.resume(value)
+        finally:
+            _active = prev_thread
+        
         return status, result
